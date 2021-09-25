55e8f4e15f958553af3a9457b834dab72c6c76ec
Eduardo Vitor Mokfa <edumokfa@hotmail.com>
2021-09-24 22:06:28 -0300

    First commit

---
+++ src/automacaoarduino/AutomacaoArduino.java
@@ -0,0 +1,16 @@
+package automacaoarduino;
+
+import conexaoArduino.conexaoArduino;
+
+/**
+ *
+ * @author Eduardo
+ */
+public class AutomacaoArduino {
+
+    conexaoArduino con = new conexaoArduino("123");
+    public static void main(String[] args) {
+       
+    }
+    
+}
